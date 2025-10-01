4cb06fb9f16fd14b5790f123b694df6b20fa2cf0
Ilya Kaznacheev <ilkaznachey@gmail.com>
2025-10-01 19:43:58 +0300

    создал проект и добавил базу

---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace screenSaver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
